be4f0ec9cdebea05d61a3d00b8787a1707196267
Ryan Michael Keller <211752292+averagebagelenjoyer@users.noreply.github.com>
2026-01-05 19:30:36 -0800

    Update PyPack.py

--- PyPack.py
+++ PyPack.py
@@ -1,1 +1,15 @@
-print("temp text")
+import re
+
+file = """
+tellraw @a "Message {"a"*100}"
+#! sleep(1s);
+tellraw @a "Message Two"
+"""
+
+# this works. don't ask how, it just does.
+file = eval(f"f'''{file.replace("\\", "\\\\").replace("'", "\\'")}'''")
+
+groups = re.search(r"(?<=^#!).*",file).groups()
+
+for match in groups:
+    print(match)
