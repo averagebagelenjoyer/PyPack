5ed3c302564b0b479845576428265288544faa9e
Ryan Michael Keller <211752292+averagebagelenjoyer@users.noreply.github.com>
2026-01-06 16:33:03 -0800

    WIP PyPack code

--- PyPack.py
+++ PyPack.py
@@ -1,15 +1,28 @@
 import re
 
+class _Internal():
+    def sleep(time=str):
+        match time[-1]:
+            case "s":
+                return f"waited {time[0:-1]} second(s)"
+            case "m":
+                return f"waited {time[0:-1]} minute(s)"
+            case "s":
+                return f"waited {time[0:-1]} tick(s)"
+
 file = """
-tellraw @a "Message {'a'*100}"
+tellraw @a "Message One"
 #! sleep(1s);
+#! sleep(2s);
 tellraw @a "Message Two"
 """
 
-# this works. don't ask how, it just does.
-file = eval(f"f'''{file.replace("\\", "\\\\").replace("'", "\\'")}'''")
+matches = re.findall(r"^#! *(.*)",file,re.MULTILINE)
 
-groups = re.search(r"(?<=^#!).*",file).groups()
-
-for match in groups:
-    print(match)
+for match in matches:
+    for command in str(match).split(";"):
+        try:
+            res = eval(f"_Internal.{command}".replace("(","('").replace(")","')"))
+            print(res)
+        except Exception as e:
+            pass
